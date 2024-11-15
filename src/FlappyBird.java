15fd00fe6fbfdf6f10c02307f0ca42683db6b91f
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-15 17:02:24 +0530

    Defining FlappyBird Method

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,5 +4,14 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird {
+public class FlappyBird extends JPanel {
+
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.blue);
+    }
+
 }
