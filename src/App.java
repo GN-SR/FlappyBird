f8a363d1a75413a9498b20da87365d90d507803a
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-15 16:57:25 +0530

    Starting defining new file

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import javax.swing.*
+import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,8 +7,10 @@ public class App {
 
         JFrame frame = new JFrame("Flappy Bird");
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
